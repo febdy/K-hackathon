cfcce9f65fc1f40ee03779373f70b2a7d195ebf5
ldy <feb29dy@gmail.com>
2016-07-01 00:50:27 +0900

    init. and added RecyclerView

---
+++ app/src/main/java/com/dayeong/gdgssu_fork/MainActivity.java
@@ -0,0 +1,59 @@
+package com.dayeong.gdgssu_fork;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        
+        ArrayList<String> recipeList = new ArrayList<String>();
+        recipeList.add("감자"); recipeList.add("고구마"); recipeList.add("호박고구마");
+        recipeList.add("오이"); recipeList.add("당근"); recipeList.add("양파");
+        recipeList.add("마늘"); recipeList.add("냠냠"); recipeList.add("맛있오");
+
+        mAdapter = new RecipeAdapter(recipeList);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/dayeong/gdgssu_fork/RecipeAdapter.java
@@ -0,0 +1,63 @@
+package com.dayeong.gdgssu_fork;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dayeong on 2016-06-30.
+ */
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+    private ArrayList<String> mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView mCardView;
+        public TextView mNumber;
+
+        public ViewHolder(View v) {
+            super(v);
+            mCardView = (CardView) v.findViewById(R.id.card);
+            mNumber = (TextView)v.findViewById(R.id.number);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public RecipeAdapter(ArrayList<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecipeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recipe, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+    //@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String recipe = mDataset.get(position);
+        holder.mNumber.setText(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
