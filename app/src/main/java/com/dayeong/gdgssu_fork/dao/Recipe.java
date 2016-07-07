3d2a65f656d24eef96a3e13508249c620b47e527
WindArrow <wind.dong.dream@gmail.com>
2016-07-07 12:29:40 +0900

    레시피 dao

--- app/src/main/java/com/dayeong/gdgssu_fork/dao/Recipe.java
+++ app/src/main/java/com/dayeong/gdgssu_fork/dao/Recipe.java
@@ -5,7 +5,37 @@ package com.dayeong.gdgssu_fork.dao;
  */
 public class Recipe {
 
-    private String title;
     private String content;
+    private String comment;
     private Timer timer;
+
+    public Recipe(String content, String comment, Timer timer) {
+        this.content = content;
+        this.comment = comment;
+        this.timer = timer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
 }
