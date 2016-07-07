a2ca166168cecc98c902d6f65e3ffcfda9f7f441
WindArrow <wind.dong.dream@gmail.com>
2016-07-07 22:20:44 +0900

    signup complete

--- app/src/main/java/com/dayeong/gdgssu_fork/network/HttpManager.java
+++ app/src/main/java/com/dayeong/gdgssu_fork/network/HttpManager.java
@@ -1,9 +1,10 @@
 package com.dayeong.gdgssu_fork.network;
 
+import android.util.Log;
+
 import com.dayeong.gdgssu_fork.dao.User;
 import com.dayeong.gdgssu_fork.utils.Global;
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONException;
@@ -18,8 +19,32 @@ public class HttpManager {
 
     private static final String TAG = "HttpManager";
 
-    public static void signup(User user, AsyncHttpResponseHandler handler) {
+    public static void signUp(User user, final HttpListener.OnSignUpListener listener) {
 
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(Global.SIGN_UP_URL, user.getRequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    Log.d(TAG, "response = " + response);
+                    boolean isSuccess = response.getBoolean("join");
+                    if (isSuccess)
+                        listener.onSuccess();
+                    else
+                        listener.onException();
+                } catch (JSONException e) {
+                    listener.onException();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                listener.onException();
+                Log.d(TAG, "통신 실패 " + statusCode);
+                Log.d(TAG, "통신 실패 " + responseString);
+            }
+        });
     }
 
     public static void login(User user, final HttpListener.OnLoginListener onLoginListener) {
